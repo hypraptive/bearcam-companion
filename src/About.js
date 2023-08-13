d1de99787f7e686dc1b7877016a82803c52388d4
Ed Miller <github@bluevalhalla.com>
2023-08-13 11:32:15 -0700

    Added AWS Hero info

--- src/About.js
+++ src/About.js
@@ -47,7 +47,6 @@ export function About ({ user }) {
     <Image width="50%" align="center"
     src="https://res.cloudinary.com/practicaldev/image/fetch/s--rw0NSRqJ--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ng4b8srovsm4aqscrck1.png" />
     <br/>
-    <br/>
     In March of 2022, one of those original engineers
     , <Link href="https://dev.to/bluevalhalla" isExternal={true}>Ed Miller <FiExternalLink/></Link>,
     was accepted into
@@ -62,6 +61,19 @@ export function About ({ user }) {
     <li>Enable experienced members of the community to share their knowledge of the bears</li>
     <li>Leverage community knowledge to teach the application to identify the bears</li>
     </ul>
+    <Image width="200px" align="center"
+    src="https://d1.awsstatic.com/getting-started-guides/new-heros-nov-2022/aws%20machine%20learning.8f879fced36fd24a30b5f9dd439bb2eb81b290a6.png" />
+    <br/>
+    In November of 2022, Ed was invited to join 
+    the <Link href="https://aws.amazon.com/developer/community/heroes/" isExternal={true}>AWS 
+    Heroes <FiExternalLink/></Link> program as
+    an <Link href="https://aws.amazon.com/developer/community/heroes/ed-miller/" isExternal={true}>AWS Machine
+    Learning Hero <FiExternalLink/></Link>. AWS credits through this program make the Bearcam Application 
+    possible! Ed's blog posts about developing the Bearcam Companion can be
+    found <Link href="https://dev.to/bluevalhalla" isExternal={true}>
+    here <FiExternalLink/></Link>.
+    <br/>
+    <br/>
     We hope you find this project beneficial. Please help us continue to improve by
     identifying bears! For information on how to contribute, see
     the <Link href="/instructions">instructions</Link> page. Happy ID-ing!
